2a71ad2722fa8a2df1d66aa9339a56a3f57ced5f
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-07 05:29:06 -0800

    init spring project layout

---
+++ api-gate/src/main/java/com/llzw/apigate/ApiGateApplication.java
@@ -0,0 +1,13 @@
+package com.llzw.apigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateApplication.class, args);
+    }
+
+}
