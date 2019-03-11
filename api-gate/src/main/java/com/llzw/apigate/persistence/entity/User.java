bda4780c7faa87914585f50069d4b571603e7be6
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-11 01:54:03 -0700

    identity info can be verified later

--- api-gate/src/main/java/com/llzw/apigate/persistence/entity/User.java
+++ api-gate/src/main/java/com/llzw/apigate/persistence/entity/User.java
@@ -1,16 +1,23 @@
 package com.llzw.apigate.persistence.entity;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class User implements UserDetails {
+public class User implements UserDetails {
 
   private static final long serialVersionUID = 1L;
 
@@ -35,13 +42,35 @@ public abstract class User implements UserDetails {
   protected String phoneNumber;
 
   @Enumerated(EnumType.STRING)
-  @Column(nullable = false, length = 10)
-  @NonNull
-  protected IdType identity_type;
+  @Column(length = 10)
+  protected IdType identityType;
 
-  @Column(nullable = false, length = 20)
+  @Column(length = 20)
+  protected String identityNumber;
+
+  @Column(length = 10)
   @NonNull
-  protected String identity_number;
+  protected String firstName;
+
+  @Column(length = 10)
+  @NonNull
+  protected String lastName;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  protected Date createdAt;
+
+  @UpdateTimestamp protected Date updatedAt;
+
+  protected boolean enabled;
+
+  protected boolean verified;
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  protected Collection<Role> roles;
 
   @Override
   public boolean isAccountNonExpired() {
@@ -60,10 +89,23 @@ public abstract class User implements UserDetails {
 
   @Override
   public boolean isEnabled() {
-    return true;
+    return enabled;
   }
 
-  enum IdType {
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream()
+        .map(Role::getPrivilegeNames)
+        .flatMap(Collection::stream)
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toList());
+  }
+
+  public Collection<String> getRoleNames() {
+    return roles.stream().map(Role::toString).collect(Collectors.toList());
+  }
+
+  public enum IdType {
     PRC_ID,
     PASSPORT
   }
