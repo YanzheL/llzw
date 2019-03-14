a5f78fd73aeedbe327bb996e5cfb84c851567dca
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-13 19:33:23 -0700

    role validation

---
+++ api-gate/src/main/java/com/llzw/apigate/web/validation/RoleConstraintValidator.java
@@ -0,0 +1,25 @@
+package com.llzw.apigate.web.validation;
+
+import com.google.common.base.Enums;
+import com.llzw.apigate.persistence.dao.RoleRepository;
+import com.llzw.apigate.persistence.entity.Role;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class RoleConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+  private RoleRepository roleRepository;
+
+  public RoleConstraintValidator(RoleRepository roleRepository) {
+    this.roleRepository = roleRepository;
+  }
+
+  @Override
+  public void initialize(final ValidPassword arg0) {}
+
+  @Override
+  public boolean isValid(final String role, final ConstraintValidatorContext context) {
+    return Enums.getIfPresent(Role.RoleType.class, role).isPresent();
+  }
+}
