b37be6ca601213c044d99f4a15704fcd36fe1950
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-30 06:42:54 -0700

    custom search criteria use JPA Specification API

---
+++ api-gate/src/main/java/com/llzw/apigate/persistence/dao/specification/SearchCriteriaSpecification.java
@@ -0,0 +1,35 @@
+package com.llzw.apigate.persistence.dao.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class SearchCriteriaSpecification<T> implements Specification<T> {
+
+  SearchCriterion criterion;
+
+  public SearchCriteriaSpecification(SearchCriterion criterion) {
+    this.criterion = criterion;
+  }
+
+  @Override
+  public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+    if (criterion.getOperation().equalsIgnoreCase(">")) {
+      return builder.greaterThanOrEqualTo(
+          root.get(criterion.getKey()), criterion.getValue().toString());
+    } else if (criterion.getOperation().equalsIgnoreCase("<")) {
+      return builder.lessThanOrEqualTo(
+          root.get(criterion.getKey()), criterion.getValue().toString());
+    } else if (criterion.getOperation().equalsIgnoreCase(":")) {
+      if (root.get(criterion.getKey()).getJavaType() == String.class) {
+        return builder.like(root.get(criterion.getKey()), "%" + criterion.getValue() + "%");
+      } else {
+        return builder.equal(root.get(criterion.getKey()), criterion.getValue());
+      }
+    }
+    return null;
+  }
+}
---
+++ api-gate/src/main/java/com/llzw/apigate/persistence/dao/specification/SearchCriterion.java
@@ -0,0 +1,15 @@
+package com.llzw.apigate.persistence.dao.specification;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor
+public class SearchCriterion {
+  private String key;
+  private String operation;
+  private Object value;
+}
