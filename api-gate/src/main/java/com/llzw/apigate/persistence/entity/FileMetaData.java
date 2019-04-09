545b5db0acd4287ee4e92ef61645aaae38b38736
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-09 05:33:36 -0700

    add FileMetaData entity

---
+++ api-gate/src/main/java/com/llzw/apigate/persistence/entity/FileMetaData.java
@@ -0,0 +1,40 @@
+package com.llzw.apigate.persistence.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor
+public class FileMetaData implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue
+  @Setter(AccessLevel.NONE)
+  protected Long id;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  protected Date createdAt;
+
+  @Column(nullable = false, updatable = false, length = 64)
+  protected String hash;
+
+  protected int referrenceCount;
+
+  @Column(nullable = false, updatable = false)
+  protected String mimetype;
+}
