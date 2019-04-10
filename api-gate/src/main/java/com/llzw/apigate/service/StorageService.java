bdd20c2dddec6a0a7dd19cb3921a5a4ee2175eed
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-10 11:17:53 +0800

    add doc

--- api-gate/src/main/java/com/llzw/apigate/service/StorageService.java
+++ api-gate/src/main/java/com/llzw/apigate/service/StorageService.java
@@ -9,10 +9,22 @@ import org.springframework.core.io.Resource;
 
 public interface StorageService {
 
+  /**
+   * Perform <tt>save</tt> operation on the target file. The actual action of <tt>save</tt> is
+   * implementation-defined.
+   *
+   * @param file The validated file container uploaded from client.
+   */
   Optional<FileMetaData> save(FileContainer file, Collection<String> msgs);
 
   Resource load(String path);
 
+  /**
+   * Perform delete operation on the target file. The actual action of <tt>delete</tt> is
+   * implementation-defined.
+   *
+   * @param path The Unique-Identifier of a file resource.
+   */
   void delete(String path);
 
 }
