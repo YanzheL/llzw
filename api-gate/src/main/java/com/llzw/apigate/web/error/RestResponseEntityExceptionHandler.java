4381ed395a4bb20d607c136b27c2678eb1b9a9c4
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-10 10:46:58 +0800

    error handle for @Valid

--- api-gate/src/main/java/com/llzw/apigate/web/error/RestResponseEntityExceptionHandler.java
+++ api-gate/src/main/java/com/llzw/apigate/web/error/RestResponseEntityExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.llzw.apigate.web.error;
 
 import com.llzw.apigate.web.util.StandardRestResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,6 +30,22 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
   protected ResponseEntity<Object> handle(Exception ex, HttpStatus status) {
     return StandardRestResponse.getResponseEntity(ex.getMessage(), false, status);
+  }
+
+  // error handle for @Valid
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatus status, WebRequest request) {
+
+    //Get all errors
+    List<String> errors = ex.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(FieldError::getDefaultMessage)
+        .collect(Collectors.toList());
+
+    return StandardRestResponse.getResponseEntity(errors, false, HttpStatus.BAD_REQUEST);
   }
 
   @Override
