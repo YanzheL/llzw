d29c2549a582f2cbe8c2c7ec51aaa2591dcdb654
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-11 05:57:55 -0700

    disable some annoying path match policy

--- api-gate/src/main/java/com/llzw/apigate/spring/RestWebConfig.java
+++ api-gate/src/main/java/com/llzw/apigate/spring/RestWebConfig.java
@@ -3,6 +3,7 @@ package com.llzw.apigate.spring;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +11,14 @@ public class RestWebConfig implements WebMvcConfigurer {
 
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    configurer
+        .defaultContentType(MediaType.APPLICATION_JSON_UTF8)
+        .favorPathExtension(false);
   }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer matcher) {
+    matcher.setUseSuffixPatternMatch(false);
+  }
+
 }
