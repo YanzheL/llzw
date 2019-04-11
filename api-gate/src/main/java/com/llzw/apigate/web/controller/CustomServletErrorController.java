0df466764da6a072c8cdc94ce8262bd7e12ae611
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-11 06:39:51 -0700

    refine Servlet Error handling

--- api-gate/src/main/java/com/llzw/apigate/web/controller/CustomServletErrorController.java
+++ api-gate/src/main/java/com/llzw/apigate/web/controller/CustomServletErrorController.java
@@ -3,29 +3,39 @@ package com.llzw.apigate.web.controller;
 import com.llzw.apigate.web.util.StandardRestResponse;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
-public class CustomServletErrorController extends BasicErrorController {
+@Controller
+@RequestMapping({"${server.error.path:${error.path:/error}}"})
+public class CustomServletErrorController extends AbstractErrorController {
 
   public CustomServletErrorController(ErrorAttributes errorAttributes) {
-    super(errorAttributes, new ErrorProperties());
+    super(errorAttributes);
+  }
+
+  @RequestMapping
+  public ResponseEntity handleError(HttpServletRequest request) {
+    return errorJson(request);
+  }
+
+  @Override
+  public String getErrorPath() {
+    return "/error";
   }
 
   @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity errorJson(HttpServletRequest request) {
-    Map<String, Object> attrs = getErrorAttributes(request,
-        isIncludeStackTrace(request, MediaType.ALL));
+    Map<String, Object> attrs = getErrorAttributes(request, false);
+    HttpStatus status = getStatus(request);
     return StandardRestResponse.errorResponseEntity(
         new Exception((String) attrs.get("message")),
-        HttpStatus.BAD_REQUEST
+        status
     );
   }
 }
--- api-gate/src/main/java/com/llzw/apigate/web/util/ApiErrorMessage.java
+++ api-gate/src/main/java/com/llzw/apigate/web/util/ApiErrorMessage.java
@@ -3,8 +3,10 @@ package com.llzw.apigate.web.util;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiErrorMessage implements Serializable {
 
