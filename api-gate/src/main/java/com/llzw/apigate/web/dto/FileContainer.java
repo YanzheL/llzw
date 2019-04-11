39ccd883e7cc9c23316fd4e4a61fde546736aa0d
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-11 05:55:26 -0700

    Supports mimeType injection to avoid Tika long time process

--- api-gate/src/main/java/com/llzw/apigate/web/dto/FileContainer.java
+++ api-gate/src/main/java/com/llzw/apigate/web/dto/FileContainer.java
@@ -1,20 +1,36 @@
 package com.llzw.apigate.web.dto;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.tika.Tika;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.multipart.MultipartFile;
 
 public class FileContainer {
 
   @Getter
-  @Setter
-  private MultipartFile file;
+  private Resource file;
 
+  @Setter
   private String mimeType = "";
 
+  @Setter
   private boolean typeDetermined;
+
+  public void setFile(MultipartFile file) {
+    this.file = file.getResource();
+  }
+
+  public void setFileFromPath(String path) throws MalformedURLException {
+    Path p = Paths.get(path);
+    this.file = new UrlResource(p.toUri());
+  }
 
   public String getMimeType() throws IOException {
     if (file == null) {
