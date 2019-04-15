69cd0bb9e8c969460b43e454b545579c29a4ddf6
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-15 15:20:31 +0800

    use AddressBean as embedded field of Order

--- api-gate/src/main/java/com/llzw/apigate/persistence/entity/Address.java
+++ api-gate/src/main/java/com/llzw/apigate/persistence/entity/Address.java
@@ -1,7 +1,5 @@
 package com.llzw.apigate.persistence.entity;
 
-import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,44 +8,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
-@Data
 @NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 @AllArgsConstructor
-public class Address implements Serializable {
-
-  private static final long serialVersionUID = 1L;
+public class Address extends AddressBean {
 
   @Id
   @GeneratedValue
   @Setter(AccessLevel.NONE)
   protected Long id;
 
+  @Getter
+  @Setter
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "ownerId")
   protected User owner;
-
-  @Column(nullable = false, length = 20)
-  @NonNull
-  protected String province;
-
-  @Column(nullable = false, length = 20)
-  @NonNull
-  protected String city;
-
-  @Column(nullable = false, length = 20)
-  @NonNull
-  protected String district;
-
-  @Column(nullable = false)
-  @NonNull
-  protected String address;
-
-  @Column(length = 6)
-  protected String zip;
 }
