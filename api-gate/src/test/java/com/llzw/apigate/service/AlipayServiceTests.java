a34b1bbdae288a573bbc1617fcd14f0270096aee
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-19 01:42:55 +0800

    use in-memory db when testing

--- api-gate/src/test/java/com/llzw/apigate/service/AlipayServiceTests.java
+++ api-gate/src/test/java/com/llzw/apigate/service/AlipayServiceTests.java
@@ -11,11 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+//@ActiveProfiles("dev")
 //@SpringBootConfiguration
 //@RunWith(SpringJUnit4ClassRunner.class)
 @ExtendWith(SpringExtension.class)
