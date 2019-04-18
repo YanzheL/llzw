2a8b7e6f2e19b352f9500c866fefb6e47ec67951
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-18 07:09:54 -0700

    add tests

--- api-gate/src/test/java/com/llzw/apigate/service/AlipayServiceTests.java
+++ api-gate/src/test/java/com/llzw/apigate/service/AlipayServiceTests.java
@@ -3,6 +3,7 @@ package com.llzw.apigate.service;
 import com.llzw.apigate.persistence.entity.Order;
 import com.llzw.apigate.persistence.entity.Payment;
 import java.lang.reflect.Field;
+import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +26,18 @@ public class AlipayServiceTests {
     Order order = new Order();
     Field idField = Order.class.getDeclaredField("id");
     idField.setAccessible(true);
-    idField.set(order, 1L);
+    idField.set(order, 2L);
     payment.setOrder(order);
     payment.setSubject("路虎");
-    payment.setTotalAmount(1000000);
+    payment.setTotalAmount(100000.1f);
     payment.setDescription("dec test");
     String orderString = alipayService.pay(payment);
     System.out.println(orderString);
+  }
+
+  @Test
+  public void queryTest() throws Exception {
+    Map<String, String> body = alipayService.query(2L);
+    System.out.println(body);
   }
 }
