f8e67f7fd584a05bf7f660d9614a654c60d5f20b
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-18 22:21:35 -0700

    add tests

--- api-gate/src/test/java/com/llzw/apigate/ApiGateApplicationTests.java
+++ api-gate/src/test/java/com/llzw/apigate/ApiGateApplicationTests.java
@@ -1,13 +1,16 @@
 package com.llzw.apigate;
 
+import com.llzw.apigate.spring.AlipayProperties;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@EnableConfigurationProperties({AlipayProperties.class})
 public class ApiGateApplicationTests {
 
   @Test
