b02671ed067c2c46f31e15801c770aeb29411c2b
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-18 22:24:39 -0700

    draft

--- api-gate/src/test/java/com/llzw/apigate/web/controller/PaymentControllerIntegerationTests.java
+++ api-gate/src/test/java/com/llzw/apigate/web/controller/PaymentControllerIntegerationTests.java
@@ -1,18 +1,14 @@
 package com.llzw.apigate.web.controller;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.llzw.apigate.ApiGateApplicationTests;
 import com.llzw.apigate.persistence.dao.UserRepository;
 import com.llzw.apigate.persistence.entity.User;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,14 +34,14 @@ public class PaymentControllerIntegerationTests extends ApiGateApplicationTests 
     mockUser = userRepository.findByUsername("test_user_customer_username_0").get();
   }
 
-  @WithUserDetails("test_user_customer_username_0")
-  @Test
-  public void createTest() throws Exception {
-    mvc.perform(
-        post("/api/v1/payments")
-            .param("orderId", "1")
-            .param("subject", "Test Subject")
-//            .with(user(userRepository.findByUsername("test_user_customer_username_0").get()))
-    ).andExpect(status().isCreated());
-  }
+//  @WithUserDetails("test_user_customer_username_0")
+//  @Test
+//  public void createTest() throws Exception {
+//    mvc.perform(
+//        post("/api/v1/payments")
+//            .param("orderId", "1")
+//            .param("subject", "Test Subject")
+////            .with(user(userRepository.findByUsername("test_user_customer_username_0").get()))
+//    ).andExpect(status().isCreated());
+//  }
 }
