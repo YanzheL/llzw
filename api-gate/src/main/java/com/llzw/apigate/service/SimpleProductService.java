282fa8b75a8f7fbc5a65eb66cce7daa1f4137e47
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-20 11:21:35 -0700

    implement file links detection

--- api-gate/src/main/java/com/llzw/apigate/service/SimpleProductService.java
+++ api-gate/src/main/java/com/llzw/apigate/service/SimpleProductService.java
@@ -1,15 +1,23 @@
 package com.llzw.apigate.service;
 
+import com.linkedin.urls.Url;
+import com.linkedin.urls.detection.UrlDetector;
+import com.linkedin.urls.detection.UrlDetectorOptions;
 import com.llzw.apigate.message.error.RestApiException;
 import com.llzw.apigate.message.error.RestDependentEntityNotFoundException;
+import com.llzw.apigate.message.error.RestEntityNotFoundException;
 import com.llzw.apigate.persistence.dao.ProductRepository;
 import com.llzw.apigate.persistence.entity.Product;
 import com.llzw.apigate.persistence.entity.User;
 import com.llzw.apigate.web.dto.ProductCreateDto;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +28,12 @@ public class SimpleProductService implements ProductService {
 
   @Setter(onMethod_ = @Autowired)
   private ProductRepository productRepository;
+
+  @Setter(onMethod_ = @Autowired)
+  private FileStorageService fileStorageService;
+
+  @Value("${spring.data.rest.base-path}")
+  private String apiBasePath;
 
   @Override
   public boolean updateValid(Long id) throws RestApiException {
@@ -33,6 +47,18 @@ public class SimpleProductService implements ProductService {
 
   @Override
   public Product create(ProductCreateDto dto, User seller) throws RestApiException {
+    if (!fileStorageService.increaseReferenceCount(dto.getCaFile())) {
+      throw new RestEntityNotFoundException(
+          String.format("CA file <%s> does not exist", dto.getCaFile()));
+    }
+    String introduction = dto.getIntroduction();
+    List<String> paths = searchFilePaths(introduction);
+    for (String path : paths) {
+      if (!fileStorageService.increaseReferenceCount(path)) {
+        throw new RestEntityNotFoundException(
+            String.format("Referenced file <%s> does not exist", path));
+      }
+    }
     Product product = new Product();
     product.setSeller(seller);
     product.setName(dto.getName());
@@ -40,6 +66,7 @@ public class SimpleProductService implements ProductService {
     product.setPrice(dto.getPrice());
     product.setCaId(dto.getCaId());
     product.setValid(true);
+    product.setCaFile(dto.getCaFile());
     return productRepository.save(product);
   }
 
@@ -51,5 +78,26 @@ public class SimpleProductService implements ProductService {
   @Override
   public List<Product> findAll(Pageable pageable) {
     return productRepository.findAll(pageable).getContent();
+  }
+
+  /**
+   * Search for file reference links used in product's introduction body. It will only detect files
+   * whose link is similar to http://xxx.com/api/v2/files/{HASH}
+   *
+   * @return File hash paths.
+   */
+  private List<String> searchFilePaths(String body) {
+    Pattern pattern = Pattern.compile(apiBasePath + "/files/([a-z0-9]{64})");
+    UrlDetector parser = new UrlDetector(body, UrlDetectorOptions.Default);
+    List<Url> found = parser.detect();
+    List<String> paths = new ArrayList<>();
+    for (Url url : found) {
+      String path = url.getPath();
+      Matcher matcher = pattern.matcher(path);
+      if (matcher.matches()) {
+        paths.add(matcher.group(1));
+      }
+    }
+    return paths;
   }
 }
