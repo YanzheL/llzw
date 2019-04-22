9f0cd0d9c016ed3cf9289081b32732f0a44b24da
Wenny <1040056148@qq.com>
2019-04-22 11:10:36 +0800

    Stock test

---
+++ api-gate/src/test/java/com/llzw/apigate/web/controller/StockControllerIntegrationTests.java
@@ -0,0 +1,86 @@
+package com.llzw.apigate.web.controller;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.llzw.apigate.ApiGateApplicationTests;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+@TestInstance(Lifecycle.PER_CLASS) //@BeforeAll和@AfterAll可以在测试接口中的静态方法上声明，也可以在接口默认方法上声明。
+@Transactional
+//@TestMethodOrder(MethodStocker.OrderAnnotation.class)
+public class StockControllerIntegrationTests extends ApiGateApplicationTests {
+  // 创建一个用于测试的mvc客户端，然后这个mvc客户端可以用来发起请求
+  @BeforeAll
+  public void setup() throws Exception {
+    mvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(springSecurity())
+        .build();
+  }
+
+  @WithUserDetails("test_user_seller_username_0")
+  @Test
+  //@Order(1) //定义了组件的加载顺序
+  public void createStock() throws Exception {
+
+    MvcResult result = mvc.perform(
+        post("/api/v1/stocks")
+            .param("productId", "1")
+            .param("producedAt", "2000-10-31T01:30:00.000-05:00")//new Date().toString())
+            .param("shelfLife", "120")
+            .param("totalQuantity", "50")
+            .param("trackingId", "1234567")
+            .param("carrierName", "运输公司")
+    )
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.success").value(true))
+        .andReturn();
+  }
+
+  @WithUserDetails("test_user_seller_username_0")
+  @Test
+  public void getStock() throws Exception {
+    MvcResult result = mvc.perform(
+        get("/api/v1/stocks/2")
+    )
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.data.id").value(2L))
+        .andReturn();
+  }
+
+  @WithUserDetails("test_user_seller_username_0")
+  @Test
+  public void searchStockByProductand() throws Exception {
+    MvcResult result = mvc.perform(
+        get("/api/v1/stocks")
+            .param("productId", "1")
+    )
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.data").isNotEmpty())
+        .andReturn();
+  }
+
+
+}
