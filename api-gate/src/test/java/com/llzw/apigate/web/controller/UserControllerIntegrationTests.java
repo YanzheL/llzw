5b504364271173aed7f2972385567542f5326ceb
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-24 09:07:13 -0700

    add tests

--- api-gate/src/test/java/com/llzw/apigate/web/controller/UserControllerIntegrationTests.java
+++ api-gate/src/test/java/com/llzw/apigate/web/controller/UserControllerIntegrationTests.java
@@ -1,9 +1,12 @@
 package com.llzw.apigate.web.controller;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.llzw.apigate.ApiGateApplicationTests;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +31,6 @@ public class UserControllerIntegrationTests extends ApiGateApplicationTests {
   @Autowired
   UserRepository userRepository;
 
-  // 创建一个用于测试的mvc客户端，然后这个mvc客户端可以用来发起请求
   @BeforeAll
   public void setup() {
     mvc = MockMvcBuilders
@@ -41,7 +44,7 @@ public class UserControllerIntegrationTests extends ApiGateApplicationTests {
     mvc.perform(
         post("/login")
             .param("username", "test_user_seller_username_0")
-            .param("password", "test_user_seller_password_0")
+            .param("password", "test_user_seller_PASSWORD_0")
     )
         .andDo(print())
         .andExpect(status().isOk())
@@ -53,7 +56,7 @@ public class UserControllerIntegrationTests extends ApiGateApplicationTests {
     mvc.perform(
         post("/login")
             .param("username", "test_user_customer_username_0")
-            .param("password", "test_user_customer_password_0")
+            .param("password", "test_user_customer_PASSWORD_0")
     )
         .andDo(print())
         .andExpect(status().isOk())
@@ -61,36 +64,15 @@ public class UserControllerIntegrationTests extends ApiGateApplicationTests {
   }
 
   @Test
-  public void registerSeller() throws Exception {
-    mvc.perform(
-        post(apiBasePath + "/users/register")
-            .param("username", "X_user_seller_username_0")
-            .param("password", "X_user_seller_password_0")
-            .param("nickname", "fff")
-            .param("email", "test1@test.org")
-            .param("phoneNumber", "12345678")
-            .param("role", "ROLE_SELLER")
-    )
-        .andDo(print())
-        .andExpect(status().isOk())
-    ;
-    User user = userRepository.findByUsername("X_user_seller_username_0").get();
-    assertNotNull(user);
-    assertNotNull(user.getCreatedAt());
-    assertNotNull(user.getUpdatedAt());
-  }
-
-  @Test
-  public void registerCustomer() throws Exception {
-    String username = "12345";
-    mvc.perform(
-        post(apiBasePath + "/users/register")
-            .param("username", username)
-            .param("password", "X_user_customer_password_0")
-            .param("nickname", "fff")
-            .param("email", "test2@test.org")
-            .param("phoneNumber", "12345678")
-            .param("role", "ROLE_SELLER")
+  public void registerValidCustomer() throws Exception {
+    String username = "a12345";
+    registerUser(
+        username,
+        "X_test_password_0",
+        null,
+        "test@test.org",
+        "12345678",
+        "ROLE_CUSTOMER"
     )
         .andDo(print())
         .andExpect(status().isOk())
@@ -99,5 +81,80 @@ public class UserControllerIntegrationTests extends ApiGateApplicationTests {
     assertNotNull(user);
     assertNotNull(user.getCreatedAt());
     assertNotNull(user.getUpdatedAt());
+  }
+
+  @Test
+  public void registerValidSeller() throws Exception {
+    String username = "a12345";
+    registerUser(
+        username,
+        "X_test_password_0",
+        null,
+        "test@test.org",
+        "12345678",
+        "ROLE_SELLER"
+    )
+        .andDo(print())
+        .andExpect(status().isOk())
+    ;
+    User user = userRepository.findByUsername(username).get();
+    assertNotNull(user);
+    assertNotNull(user.getCreatedAt());
+    assertNotNull(user.getUpdatedAt());
+  }
+
+  @Test
+  public void registerInvalidRole() throws Exception {
+    String username = "a12345";
+    registerUser(
+        username,
+        "X_test_password_0",
+        null,
+        "test@test.org",
+        "12345678",
+        "ROLE_XXX"
+    )
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+    ;
+    assertFalse(userRepository.findByUsername(username).isPresent());
+  }
+
+  @Test
+  public void registerInvalidEmailAndRoleAndNickname() throws Exception {
+    String username = "a12345";
+    registerUser(
+        username,
+        "X_test_password_0",
+        "asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+        "test@testorg",
+        "12345678",
+        "ROLE_XXX"
+    )
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.error.message").value(containsString("email")))
+        .andExpect(jsonPath("$.error.message").value(containsString("role")))
+        .andExpect(jsonPath("$.error.message").value(containsString("nickname")))
+    ;
+    assertFalse(userRepository.findByUsername(username).isPresent());
+  }
+
+  private ResultActions registerUser(
+      String username,
+      String password,
+      String nickname,
+      String email,
+      String phoneNumber, String role
+  ) throws Exception {
+    return mvc.perform(
+        post(apiBasePath + "/users/register")
+            .param("username", username)
+            .param("password", password)
+            .param("nickname", nickname)
+            .param("email", email)
+            .param("phoneNumber", phoneNumber)
+            .param("role", role)
+    );
   }
 }
