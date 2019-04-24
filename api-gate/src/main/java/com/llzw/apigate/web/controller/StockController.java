3ea398f3df73cbc6765c67ec7c27b1539e6dbb7a
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-24 08:35:35 -0700

    add @Validated

--- api-gate/src/main/java/com/llzw/apigate/web/controller/StockController.java
+++ api-gate/src/main/java/com/llzw/apigate/web/controller/StockController.java
@@ -26,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @BasePathAwareController
 @RequestMapping(value = "/stocks")
