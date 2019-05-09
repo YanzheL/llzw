c1211e10cbe2e0c90f918294f1e7911d18fa8a83
YanzheL <lee.yanzhe@yanzhe.org>
2019-05-09 14:33:28 +0800

    Remove @EnableWebMvc

--- api-gate/src/main/java/com/llzw/apigate/spring/RestWebConfig.java
+++ api-gate/src/main/java/com/llzw/apigate/spring/RestWebConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class RestWebConfig implements WebMvcConfigurer {
 
   @Override
