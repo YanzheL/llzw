70356ef6a870d2691cdbe782228dcc50c75bc7f6
YanzheL <lee.yanzhe@yanzhe.org>
2019-05-22 20:22:59 -0700

    Inherit Exception class instead of RuntimeException

--- api-gate/src/main/java/com/llzw/apigate/message/error/RestApiException.java
+++ api-gate/src/main/java/com/llzw/apigate/message/error/RestApiException.java
@@ -3,7 +3,7 @@ package com.llzw.apigate.message.error;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class RestApiException extends RuntimeException implements RestApiErrorMessage {
+public class RestApiException extends Exception implements RestApiErrorMessage {
 
   private final static String type = "API";
 
