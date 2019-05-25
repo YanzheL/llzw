af085ab16e86a640c7fa090672e116778a7d41b8
YanzheL <lee.yanzhe@yanzhe.org>
2019-05-25 01:33:28 -0700

    OS Env based configuration

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,16 +1,19 @@
+const api_host = process.env.API_HOST || 'http://localhost:8981';
+const api_base_path = process.env.API_BASE_PATH || '/api/v1';
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
-    authDomain: "embryo-version-2.firebaseapp.com",
-    databaseURL: "https://embryo-version-2.firebaseio.com",
-    projectId: "embryo-version-2",
-    storageBucket: "embryo-version-2.appspot.com",
-    messagingSenderId: "73552048992"
-  },
-  // url1: 'http://localhost:8981/api/v1',
-  url1: 'https://llzw.hitnslab.com/api/v1',
-
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
+        authDomain: "embryo-version-2.firebaseapp.com",
+        databaseURL: "https://embryo-version-2.firebaseio.com",
+        projectId: "embryo-version-2",
+        storageBucket: "embryo-version-2.appspot.com",
+        messagingSenderId: "73552048992",
+    },
+    // url1: 'http://localhost:8981/api/v1',
+    apiHost: api_host,
+    apiBasePath: api_base_path,
+    url1: api_host + api_host,  // 后端API
 };
 
 
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,19 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+const api_host = process.env.API_HOST || 'http://localhost:8981';
+const api_base_path = process.env.API_BASE_PATH || '/api/v1';
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
-    authDomain: "embryo-version-2.firebaseapp.com",
-    databaseURL: "https://embryo-version-2.firebaseio.com",
-    projectId: "embryo-version-2",
-    storageBucket: "embryo-version-2.appspot.com",
-    messagingSenderId: "73552048992",
-  },
-  // url1: 'http://localhost:8981/api/v1',
-  url1: 'https://llzw.hitnslab.com/api/v1',  // 后端API
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
+        authDomain: "embryo-version-2.firebaseapp.com",
+        databaseURL: "https://embryo-version-2.firebaseio.com",
+        projectId: "embryo-version-2",
+        storageBucket: "embryo-version-2.appspot.com",
+        messagingSenderId: "73552048992",
+    },
+    // url1: 'http://localhost:8981/api/v1',
+    apiHost: api_host,
+    apiBasePath: api_base_path,
+    url1: api_host + api_host,  // 后端API
 };
 
 export const seller_apiUrl = environment.url1;
